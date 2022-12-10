c9416f929ac2c33ddcf1717e6f17ad1a9a0a6c3e
lixizan <lixizan@webull.com>
2022-12-10 19:23:43 +0800

    默认工作队列的错误处理

--- options.go
+++ options.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"runtime"
 	"strings"
 )
@@ -17,11 +18,17 @@ var (
 	}
 
 	// 默认工作队列, 64协程, 开启恢复模式
-	DefaultWorker = NewWorkerQueue(
+	DefaultWorkerQueue = NewWorkerQueue(
 		WithConcurrency(64),
 		WithRecovery(),
 	)
 )
+
+func init() {
+	DefaultWorkerQueue.OnError = func(err error) {
+		log.Printf(err.Error())
+	}
+}
 
 type (
 	Job struct {
@@ -73,15 +80,15 @@ func WithRecovery() Option {
 		c.Caller = func(job Job) (err error) {
 			defer func() {
 				if fatalError := recover(); fatalError != nil {
-					var msg = make([]byte, 0, 128)
+					var msg = make([]byte, 0, 256)
 					msg = append(msg, fmt.Sprintf("fatal error: %v\n", fatalError)...)
 					for i := 1; true; i++ {
-						if _, caller, line, ok := runtime.Caller(i); ok {
-							if !strings.Contains(caller, "src/runtime") {
-								msg = append(msg, fmt.Sprintf("caller: %s, line: %d\n", caller, line)...)
-							}
-						} else {
+						_, caller, line, ok := runtime.Caller(i)
+						if !ok {
 							break
+						}
+						if !strings.Contains(caller, "src/runtime") {
+							msg = append(msg, fmt.Sprintf("caller: %s, line: %d\n", caller, line)...)
 						}
 					}
 					err = errors.New(string(msg))
